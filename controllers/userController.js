ad7c12411bcf3047609c391f2784228e6d8c1307
andres085 <andresito.martinez1985@gmail.com>
2022-04-05 10:57:05 -0300

    Crud de users funcionando

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,23 +3,57 @@ const userService = require('../services/userService');
 module.exports.getAll = async (req, res) => {
     try {
         const users = await userService.getAll();
-
         return res.status(200).send(users);
     } catch (error) {
-        return res.status(500).send({
-            error: "Couldn't find any users"
+        return res.status(error.status).send({
+            error: error.message
         })
     }
 }
 
 module.exports.getOne = async (req, res) => {
+    const { id } = req.params;
     try {
-        const user = await userService.getOne(req.params.id);
-
+        const user = await userService.getOne(id);
         return res.status(200).send(user);
     } catch (error) {
-        return res.status(500).send({
-            error: "Couldn't find any user"
+        return res.status(error.status).send({
+            error: error
         });
+    }
+}
+
+module.exports.createOne = async (req, res) => {
+    try {
+        const user = await userService.createOne(req.body);
+        return res.status(200).send(user);
+    } catch (error) {
+        return res.status(error.status).send({
+            error: error
+        })
+    }
+}
+
+module.exports.updateOne = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const user = await userService.updateOne(id, req.body);
+        return res.status(200).send(user);
+    } catch (error) {
+        return res.status(error.status).send({
+            error: error
+        })
+    }
+}
+
+module.exports.deleteOne = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const user = await userService.deleteOne(id);
+        return res.status(200).send(user);
+    } catch (error) {
+        return res.status(error.status).send({
+            error: error
+        })
     }
 }
