19379f9b481bee9334bcfc490d3686ea673e0c0d
andres085 <andresito.martinez1985@gmail.com>
2022-05-04 20:27:09 -0300

    Se modifico la variable de entorno del puerto

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/', apiRouter);
 
-const PORT = process.env.NODE_DOCKER_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}.`);
